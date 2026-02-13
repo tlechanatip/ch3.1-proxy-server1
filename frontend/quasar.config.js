8c7e1fb572d768079bdcff8e3d28cc9d9ef855a3
tlechanatip <chanatipsoyjit1@gmail.com>
2026-02-13 18:40:52 +0700

    quasar.config.js

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -37,7 +37,7 @@ export default defineConfig((ctx) => {
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
       env: {
-        API_URL: 'https://ch3-1-proxy-server.onrender.com/api'
+        API_URL: 'https://ch3-1-proxy-server.onrender.com/api&#39'
       },
       // publicPath: '/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
